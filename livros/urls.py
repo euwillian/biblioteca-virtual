4bebd133b6b0dda5c17dce881fcbec170f6b8e89
Willian Santos <12131283+euwillian@users.noreply.github.com>
2025-06-23 19:50:26 -0300

    feat: implementa todas as operações CRUD para o cadastro de livros

--- livros/urls.py
+++ livros/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from livros.views import livro
+from livros.views import livro, editar_livro, excluir_livro
 
 urlpatterns = [
     path('novo_livro/', livro, name='novo_livro'),
+    path('editar_livro/<int:id>', editar_livro, name='editar_livro'),
+    path('excluir_livro/<int:id>', excluir_livro, name='excluir_livro'),
 ]
+
+"""
+
+Quando você define <int:alguma_coisa> na URL, o Django passa isso como argumento nomeado para a sua função.
+
+Se estiver <int:livro_id>, o Django faz:
+
+editar_livro(request, livro_id=123)
+Então sua função precisa aceitar exatamente esse nome (livro_id), ou dará erro.
+
+
+"""
--- livros/views.py
+++ livros/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Livro
 
 def livro(request):
@@ -10,5 +10,47 @@ def livro(request):
         
         novo_livro = Livro(titulo=titulo, autor=autor, ano=ano, categoria=categoria)
         novo_livro.save()
+        
+    listar_livros = Livro.objects.all()
+    # Busca todos livros
+        
+    contexto = {
+        'livros': listar_livros,
+        }
+        
+    return render(request, 'novo_livro.html', contexto)
+
+
+def editar_livro(request, id):
+    livro = get_object_or_404(Livro, id=id)
     
-    return render(request, 'novo_livro.html')
+    if request.method == 'POST':
+        # pegar tudo que o usuário esta enviado no POST via GET
+        livro.titulo = request.POST.get("titulo")
+        livro.autor = request.POST.get("autor")
+        livro.ano = request.POST.get("ano")
+        livro.categoria = request.POST.get("categoria")
+        livro.save()
+        
+        return redirect('novo_livro') # volta para a página principal
+    
+    contexto = {'livro': livro,}
+    
+    return render(request, 'editar_livro.html', contexto)
+
+
+def excluir_livro(request, id):
+    livro = get_object_or_404(Livro, id=id)
+    
+    if request.method == 'POST':
+        # pegar tudo que o usuário esta enviado no POST via GETlivro.titulo = request.POST.get("titulo")
+        livro.autor = request.POST.get("autor")
+        livro.ano = request.POST.get("ano")
+        livro.categoria = request.POST.get("categoria")
+        livro.delete()
+        
+        return redirect('novo_livro') # volta para a página principal
+    
+
+    contexto = {'livro': livro,}
+    return render(request, 'excluir_livro.html', contexto)
